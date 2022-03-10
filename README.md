4d6616dc293c860579716fba11a54d0332fbcc38
zoltanszikszo <szikszo.zoltan@gmail.com>
2022-03-10 13:01:38 +0100

    Localhost connection with Knex, two get response from the database for test

---
+++ README.md
@@ -0,0 +1,6 @@
+# swp22k-101admin-back
+
+* .env set and taken into use (ports and other info still have to be checked though)
+* nodemon correct version is taken into use (the tool that restarts the server after any change to the code is saved)
+* npm run dev  - command to start the server
+
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const cors = require('cors');
+
+dotenv.config();
+
+const knex = require('knex')({
+  client: process.env.DB_DRIVER_MODULE || 'mysql',
+  connection: {
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_POR) || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    debug: process.env.DB_DEBUG || false,
+    multipleStatements: process.env.DB_MULTIPLE_STATEMENTS || true,
+  },
+  pool: {
+    min: Number(process.env.DB_CONNECTION_POOL_MIN) || 0,
+    max: Number(process.env.DB_CONNECTION_POOL_MAX) || 7,
+  }
+})
+
+app.use(cors())
+
+let confirmations = [{
+  uuid: "00816d86eae9ee6534d0d03f",
+  pickup_time: "2022-03-01 08:22:13"
+}]
+
+app.get('/confirmations/all', (request, response) => {
+  knex
+    .select()
+    .from("signature")
+    .then(data => {
+      response.json(data);
+    })
+    .catch(error => {
+      console.log(error);
+
+    })
+})
+
+app.get('/confirmations/:id', (request, response) => {
+  knex
+    .select()
+    .from("signature")
+    .where("uuid", request.params.id)
+    .then(data => {
+      response.json(data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+})
+
+app.listen(process.env.BE_SERVER_PORT);
+console.log(`Node server started and listens to port ${process.env.BE_SERVER_PORT}.`);
